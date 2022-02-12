e67b07593f553bec06422df324cac46aca06985c
garfield <admin@g4rf.net>
2022-02-12 22:58:15 +0100

    disabled server automatic reload

--- server.sh
+++ server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./node_modules/live-server/live-server.js --https=./node_modules/live-server-https --open=/animation
+# server with https and ignore changes at all
+./node_modules/live-server/live-server.js --https=./node_modules/live-server-https --ignore=.
